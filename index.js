ca71b3a99626bf5c0ee3b19c4a8aaa240c7fc082
Nitesh Patel <72400641+niteshxp@users.noreply.github.com>
2023-01-20 15:32:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const counter = document.getElementById('counter')
+const btns = document.querySelectorAll('.btn')
+
+let count = 0
+
+btns.forEach((btn) => {
+    btn.addEventListener('click',(e) => {
+        const styles = e.currentTarget.classList
+        if(styles.contains('increase')){
+            count++
+        } else if (styles.contains('decrease')){
+            count--
+        }
+        else {
+            count = 0
+        }
+
+        if (count>0) {
+            counter.style.color ='blue'
+        }
+        
+        else if (count <0) {
+            counter.style.color = 'red'
+
+        } else {
+            counter.style.color ='black'
+        }
+        counter.innerHTML = count
+    })
+    
+});
